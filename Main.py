0eba59292dcc3532f90ba7d846bc3bac70a0915d
Lailyadhimah <lailyfarkhaha@gmail.com>
2023-10-21 10:28:02 +0700

    menambahkan fungsi perkalian dan pembagian

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
-from math_function import add
-
+from Math_function import add
+from Math_function import multiply
+from Math_function import divide
 
 def main():
 
@@ -9,6 +10,13 @@ def main():
 
     if operator == "+":
         result = add(data_1, data_2)
+    elif operator == "*":
+        result = multiply(data_1, data_2)
+    elif operator == "/":
+        if data_2 == 0:
+            print("Pembagian dengan nol tidak diizinkan.")
+        else:
+            result = divide(data_1, data_2)
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
